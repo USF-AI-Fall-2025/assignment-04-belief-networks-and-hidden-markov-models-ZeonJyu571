40a73b368261d3e125caf426bacf5f4351c6a69f
ZeonJyu571 <jmeng15@dons.usfca.edu>
2025-11-17 17:31:29 -0800

    Add files via upload

--- alarm.py
+++ alarm.py
@@ -1,7 +1,7 @@
-from pgmpy.models import BayesianNetwork
+from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.inference import VariableElimination
 
-alarm_model = BayesianNetwork(
+alarm_model = DiscreteBayesianNetwork(
     [
         ("Burglary", "Alarm"),
         ("Earthquake", "Alarm"),
@@ -27,7 +27,7 @@ cpd_alarm = TabularCPD(
     values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
     evidence=["Burglary", "Earthquake"],
     evidence_card=[2, 2],
-    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['yes','no']},
+    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['no','yes']},
 )
 cpd_johncalls = TabularCPD(
     variable="JohnCalls",
@@ -35,15 +35,15 @@ cpd_johncalls = TabularCPD(
     values=[[0.95, 0.1], [0.05, 0.9]],
     evidence=["Alarm"],
     evidence_card=[2],
-    state_names={"Alarm":['yes','no'], "JohnCalls":['yes', 'no']},
+    state_names={"Alarm":['no','yes'], "JohnCalls":['no', 'yes']},
 )
 cpd_marycalls = TabularCPD(
     variable="MaryCalls",
     variable_card=2,
-    values=[[0.1, 0.7], [0.9, 0.3]],
+    values=[[0.99, 0.3], [0.01, 0.7]],
     evidence=["Alarm"],
     evidence_card=[2],
-state_names={"Alarm":['yes','no'], "MaryCalls":['yes', 'no']},
+    state_names={'Alarm':['no','yes'], 'MaryCalls':['no', 'yes']},
 )
 
 # Associating the parameters with the model structure
@@ -53,8 +53,17 @@ alarm_model.add_cpds(
 alarm_infer = VariableElimination(alarm_model)
 
 #print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
-#
-#the probability of Mary Calling given that John called
 
-q = alarm_infer.query(variables=["Alarm", "Burglary"],evidence={"MaryCalls":"yes"})
-print(q)
+def main():
+    print("1. The probability of Mary Calling given that John called")
+    print(alarm_infer.query(["MaryCalls"], {"JohnCalls":"yes"}))
+
+    print("2. The probability of both John and Mary calling given Alarm")
+    print(alarm_infer.query(["MaryCalls", "JohnCalls"], {"Alarm": "yes"}))
+
+    print("3. The probability of Alarm, given that Mary called")
+    print(alarm_infer.query(["Alarm"],{"MaryCalls": "yes"}))
+
+if __name__ == "__main__":
+    main()
+
