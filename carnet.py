6f87ba4f968e86c0c8cac6406dd726d3c774bd5f
ZeonJyu571 <jmeng15@dons.usfca.edu>
2025-11-17 17:32:34 -0800

    Add files via upload

--- carnet.py
+++ carnet.py
@@ -1,8 +1,8 @@
-from pgmpy.models import BayesianNetwork
+from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.inference import VariableElimination
 from pgmpy.factors.discrete import TabularCPD
 
-car_model = BayesianNetwork(
+car_model = DiscreteBayesianNetwork(
     [
         ("Battery", "Radio"),
         ("Battery", "Ignition"),
@@ -66,6 +66,53 @@ car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, c
 
 car_infer = VariableElimination(car_model)
 
-print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
+#print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
 
+def main():
+    print("\n 1. Given that the car will not move, what is the probability that the battery is not working?")
+    result1 = car_infer.query(["Battery"], {"Moves": "no"})
+    P_not_B_given_not_M = result1.values[1]
+    print(P_not_B_given_not_M)
 
+    print("\n 2. Given that the radio is not working, what is the probability that the car will not start?")
+    result2 = car_infer.query(["Starts"], {"Radio": "Doesn't turn on"})
+    P_not_S_given_not_R = result2.values[1]
+    print(P_not_S_given_not_R)
+
+    print("\n 3. Given that the battery is working, does the probability of the radio working change if we discover that the car has gas in it?")
+    print(" 1) P(R|B, G)")
+    result3 = car_infer.query(["Radio"], {"Battery": "Works", "Gas": "Full"})
+    P_R_given_B_and_G = result3.values[0]
+    print(P_R_given_B_and_G)
+    print(" 2) P(R|B, not G)")
+    result4 = car_infer.query(["Radio"], {"Battery": "Works", "Gas": "Empty"})
+    P_R_given_B_and_not_G = result4.values[0]
+    print(P_R_given_B_and_not_G)
+    print(" 3) Whether P(R|B, G) is equal to P(R|B)?")
+    print(P_R_given_B_and_not_G == P_R_given_B_and_G)
+
+    print("\n 4. Given that the car doesn't move, how does the probability of the ignition failing change if we observe that the car does not have gas in it?")
+    print(" 1) P(not I|not M, not G)")
+    result5 = car_infer.query(["Ignition"], {"Moves": "no", "Gas": "Empty"})
+    P_not_I_given_not_M_and_not_G = result5.values[1]
+    print(P_not_I_given_not_M_and_not_G)
+    print(" 2) P(not I|not M, G)")
+    result6 = car_infer.query(["Ignition"], {"Moves": "no", "Gas": "Full"})
+    P_not_I_given_not_M_and_G = result6.values[1]
+    print(P_not_I_given_not_M_and_G)
+    print(" 3) Change")
+    change = P_not_I_given_not_M_and_G - P_not_I_given_not_M_and_not_G
+    if change == 0:
+        print("The probability does not change")
+    elif change > 0:
+        print(f"The probability decreases {change}")
+    else:
+        print(f"The probability increases {abs(change)}")
+
+    print("\n 5. What is the probability that the car starts if the radio works and it has gas in it?")
+    result7 = car_infer.query(["Starts"], {"Radio": "turns on", "Gas": "Full"})
+    P_S_given_R_and_G = result7.values[0]
+    print(P_S_given_R_and_G)
+
+if __name__ == "__main__":
+    main()
