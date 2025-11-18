646c0b7312817304cdc911a48b638df4b1a1dd35
ZeonJyu571 <jmeng15@dons.usfca.edu>
2025-11-17 17:32:47 -0800

    Add files via upload

---
+++ carnet task 3.py
@@ -0,0 +1,87 @@
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.inference import VariableElimination
+from pgmpy.factors.discrete import TabularCPD
+
+car_model = DiscreteBayesianNetwork(
+    [
+        ("Battery", "Radio"),
+        ("Battery", "Ignition"),
+        ("Ignition","Starts"),
+        ("Gas","Starts"),
+        ("KeyPresent","Starts"),
+        ("Starts","Moves")
+])
+
+# Defining the parameters using CPT
+
+
+cpd_battery = TabularCPD(
+    variable="Battery", variable_card=2, values=[[0.70], [0.30]],
+    state_names={"Battery":['Works',"Doesn't work"]},
+)
+
+cpd_gas = TabularCPD(
+    variable="Gas", variable_card=2, values=[[0.40], [0.60]],
+    state_names={"Gas":['Full',"Empty"]},
+)
+
+cpd_radio = TabularCPD(
+    variable=  "Radio", variable_card=2,
+    values=[[0.75, 0.01],[0.25, 0.99]],
+    evidence=["Battery"],
+    evidence_card=[2],
+    state_names={"Radio": ["turns on", "Doesn't turn on"],
+                 "Battery": ['Works',"Doesn't work"]}
+)
+
+cpd_ignition = TabularCPD(
+    variable=  "Ignition", variable_card=2,
+    values=[[0.75, 0.01],[0.25, 0.99]],
+    evidence=["Battery"],
+    evidence_card=[2],
+    state_names={"Ignition": ["Works", "Doesn't work"],
+                 "Battery": ['Works',"Doesn't work"]}
+)
+
+cpd_starts = TabularCPD(
+    variable="Starts",
+    variable_card=2,
+    values=[[0.99, 0.01, 0.01, 0.001, 0.01, 0.01, 0.01, 0.01], [0.01, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99]],
+    evidence=["Ignition", "Gas", "KeyPresent"],
+    evidence_card=[2, 2, 2],
+    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"], "KeyPresent":["yes","no"]},
+)
+
+cpd_moves = TabularCPD(
+    variable="Moves", variable_card=2,
+    values=[[0.8, 0.01],[0.2, 0.99]],
+    evidence=["Starts"],
+    evidence_card=[2],
+    state_names={"Moves": ["yes", "no"],
+                 "Starts": ['yes', 'no'] }
+)
+
+cpd_key = TabularCPD(
+    variable = "KeyPresent",
+    variable_card = 2,
+    values = [[0.70],[0.30]],
+    state_names = {"KeyPresent": ["yes", "no"]}
+)
+
+
+# Associating the parameters with the model structure
+car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves, cpd_key)
+
+car_infer = VariableElimination(car_model)
+
+# print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
+
+def main():
+    result = car_infer.query(["KeyPresent"], {"Moves":"no"})
+    P_not_K_given_not_M = result.values[1]
+    print("the probability that the key is not present given that the car does not move:")
+    print(P_not_K_given_not_M)
+
+if __name__ == "__main__":
+    main()
+
